27f2942b94fc4c32c1f3a88159137bc3ece14de9
Christoph Wille <christophw@alphasierrapapa.com>
2012-10-07 17:36:14 +0200

    Reading appxmanifest version information

--- Source/Sprudelsuche/AboutUserControl.xaml.cs
+++ Source/Sprudelsuche/AboutUserControl.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -29,6 +30,17 @@ namespace Sprudelsuche
         private void PrivacyPolicyRefClick(object sender, RoutedEventArgs e)
         {
             Windows.System.Launcher.LaunchUriAsync(new Uri(Constants.PrivacyPolicyUrl));
+        }
+
+        // http://www.michielpost.nl/PostDetail_67.aspx
+        private string GetAppVersion()
+        {
+
+            Package package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+
+            return string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
         }
     }
 }
